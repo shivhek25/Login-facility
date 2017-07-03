60503471fd29e20eed670c7f96dfdc7d8aa98210
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-07-03 14:25:31 +0530

    Updated data with login credentials

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'Rvm@i[9)0?~=');
+$connection = mysqli_connect('localhost:8889', 'root', 'root');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'login');
+$select_db = mysqli_select_db($connection, 'Login_facility');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }?>
