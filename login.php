7076f2308f8a93ea7e488790aa5a1eb0507d0223
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-07-03 20:09:51 +0530

    Added login page and back-end

---
+++ login.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+	require('connect.php');
+    // If the values are posted, insert them into the database.
+    if (isset($_POST['username']) && isset($_POST['password'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+    $sql= "SELECT * FROM 'login' WHERE username='$username' AND password='$password'";
+    $result= mysqli_query($connection, $sql);
+    $count = mysqli_num_rows($result);
+    if($count==1){
+     $_SESSION['username']=$username;
+    }
+    else {
+      echo "Invalid Username or Password";
+    }
+  }
+  if(isset($_SESSION['username'])){
+   echo "User already logged in";
+ }
+    ?>
+<html>
+  <head>
+    <title>User Login in php and mysql </title>
+<!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+<!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+  <link rel="stylesheet" href="styles.css" >
+
+<!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>{
+    body {
+      padding-top: 40px;
+      padding-bottom: 40px;
+      background-color: #eee;
+      }
+
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+}</style>
+</head>
+<body>
+<div class="container">
+      <form class="form-signin" method="POST">
+        <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+        <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+        <h2 class="form-signin-heading">Please Register</h2>
+        <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
+	  <input type="text" name="username" class="form-control" placeholder="Username" required>
+	</div>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+        <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>
+      </form>
+</div>
+</body>
+</html>
